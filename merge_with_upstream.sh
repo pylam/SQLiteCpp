6cbf125263e23ae8479b8c3365544e4facfb1325
Alan Lam <git@syssolhk.com>
2019-04-03 07:22:39 +0100

    add script to merge with upstream

---
+++ merge_with_upstream.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#https://help.github.com/en/articles/merging-an-upstream-repository-into-your-fork
+
+git pull https://github.com/SRombauts/SQLiteCpp.git master
+git status
+git push origin master
